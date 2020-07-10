19904fc63a833c5094b34e53a88046121494a190
rafaelreisramos <rrramos@gmail.com>
2020-07-09 22:39:25 -0300

    Transactions CRUD

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,23 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (
+        acumulator: Omit<Balance, 'total'>,
+        transaction: Transaction,
+      ): Omit<Balance, 'total'> => {
+        const sum = acumulator;
+        sum[transaction.type] += Number(transaction.value);
+        return sum;
+      },
+      { income: 0, outcome: 0 },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
